350bbfd1b803ae3b10cc241d1f0f4b500ab41b5e
Nishithcs <nishithcs@gmail.com>
2025-05-10 20:23:21 +0530

    added tests

--- tests/transaction_history_handler_test.go
+++ tests/transaction_history_handler_test.go
@@ -9,39 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 	"net/http/httptest"
 )
-
-// MockMongoDBClient is a mock implementation of the MongoDBClient interface
-type MockMongoDBClient struct {
-	mock.Mock
-}
-
-func (m *MockMongoDBClient) Insert(ctx context.Context, collection string, document interface{}) (*mongo.InsertOneResult, error) {
-	args := m.Called(ctx, collection, document)
-	return args.Get(0).(*mongo.InsertOneResult), args.Error(1)
-}
-
-// func (m *MockMongoDBClient) Find(ctx context.Context, collection string, filter interface{}) (*mongo.Cursor, error) {
-// 	args := m.Called(ctx, collection, filter)
-// 	return args.Get(0).(*mongo.Cursor), args.Error(1)
-// }
-func (m *MockMongoDBClient) Find(ctx context.Context, collection string, filter interface{}) (*mongo.Cursor, error) {
-	args := m.Called(ctx, collection, filter)
-
-	var cursor *mongo.Cursor
-	if args.Get(0) != nil {
-		cursor = args.Get(0).(*mongo.Cursor)
-	}
-	return cursor, args.Error(1)
-}
-
-func (m *MockMongoDBClient) Disconnect(ctx context.Context) error {
-	args := m.Called(ctx)
-	return args.Error(0)
-}
 
 
 // TestGetTransactions_MongoDBFailure tests the case when MongoDB fails to return results
